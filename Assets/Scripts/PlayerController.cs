c7db44f79d281147a3e53966ab656703ae0d6a95
jimin1231 <polly12312@naver.com>
2019-11-29 02:23:38 +0900

    기본 캐릭터 움직이기

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+    [SerializeField]
+    private float walkSpeed;
+
+    [SerializeField]
+    private float lookSensitivity; //카메라 민감도
+
+    [SerializeField]
+    private float cameraRotationLimit; //상하 카메라 각도 제한
+
+    private float currentCameraRotationX = 0; //상하 카메라 회전
+
+    [SerializeField]
+    private Camera theCamera;
+
+    private Rigidbody myRigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>(); //인스펙터 창의 rigidbody 가져오기
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CameraRotation();
+        CharacterRotation();
+    }
+
+    private void Move()
+    {
+        float _moveDirX = Input.GetAxisRaw("Horizontal");
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _moveDirX;
+        Vector3 _moveVertical = transform.forward * _moveDirZ;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+
+        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+    }
+
+    //좌우 캐릭터회전
+    private void CharacterRotation()
+    {
+        float _yRotation = Input.GetAxis("Mouse X");
+        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
+        //구한 벡터값을 Quaternion값으로 변환 후 myRigid.rotation과 곱
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));
+    }
+
+    //상하 카메라 회전
+    private void CameraRotation()
+    {
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+        float _cameraRotationX = (_xRotation * lookSensitivity);
+        currentCameraRotationX -= _cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit); //가두기
+
+        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+    }
+
+    
+}
