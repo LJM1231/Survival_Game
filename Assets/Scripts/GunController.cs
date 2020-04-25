9f59bf9bc9b887a68ac0674f8a070b55bd696431
jimin1231 <polly12312@naver.com>
2020-04-25 18:02:05 +0900

    총 구현 완료

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField]
+    private Gun currentGun;
+
+    private float currentFireRate;
+
+    private AudioSource AudioSource;
+
+    private void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GunFireRateCalc();
+        TryFire();
+    }
+    private void GunFireRateCalc()
+    {
+        if (currentFireRate > 0)
+            currentFireRate -= Time.deltaTime; // 1/60=1 //1초에 1씩 감소
+    }
+
+    private void TryFire()
+    {
+        if (Input.GetButton("Fire1") && currentFireRate <= 0)
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        currentFireRate = currentGun.fireRate;
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        PlaySE(currentGun.fire_Sound);
+        currentGun.muzzleFlash.Play();
+        Debug.Log("총알 발사함");
+
+    }
+
+    private void PlaySE(AudioClip _clip)
+    {
+        AudioSource.clip = _clip;
+        AudioSource.Play();
+    }
+}
